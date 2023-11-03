c826e1af3dd38b3654acefe96eada2fc0f449247
Rybka <melmochalka123@gmail.com>
2023-11-03 14:56:28 +0300

    Commit of lesha

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Привет мир");
+        System.out.println("Какая-нибудь строчка");
         System.out.println("Пока мир");
     }
 }
